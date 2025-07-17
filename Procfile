d6203cfa22e159f6eaf06f6d14bd6f6326b47501
DanielMacau <d.macau@campus.fct.unl.pt>
2025-07-17 15:11:22 +0100

    Heroku connection

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm --prefix ./main start
+web: npm install && npm start
--- main/src/api/models/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Component';
-export * from './ExpComponent';
-export * from './MarkComponent';
-export * from './RuleComponent';
-export * from './TreeComponent';
